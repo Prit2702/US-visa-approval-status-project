d73c95a7c821604e60efa24a75d2d45ef9d9e79b
Pritish <prit.deshmukh1803@gmail.com>
2025-08-13 16:48:22 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Pritish",
+    author_email="prit.deshukh1803@gmail.com",
+    packages=find_packages()   # Automatically find packages in the project
+)
